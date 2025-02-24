56fd686136bf158f87e63ef868faf7302b23a6a3
karthikeya-aduri <131266273+karthikeya-aduri@users.noreply.github.com>
2025-02-24 19:52:58 +0530

    The app can now be used for deleting pages from a pdf. Added a getMetadata function.

--- handler.py
+++ handler.py
@@ -1,5 +1,5 @@
 from constants import hoverColor, backgroundColor, eventMap
-from tkinter.filedialog import askopenfilename, askopenfilenames
+from tkinter.filedialog import askopenfilename, askopenfilenames, asksaveasfilename
 import tkinter.messagebox as tkmb
 import PyPDF2 as pydf
 import re
@@ -34,44 +34,63 @@ def addPDF(pdf, label):
 
 def parseEntry(entry):
     pagesEntry = entry.get().replace(' ', '')
-    pattern = r"^\d+(, \d+)*$"
+    pattern = r"^\d+(?:-\d+)?(?:,\d+(?:-\d+)?)*$"
     match = re.fullmatch(pattern, pagesEntry)
     pages = []
     if match:
         points = pagesEntry.split(',')
         for point in points:
             if '-' in point:
-                index = point.find('-')
-                startPage = int(point[:index])
-                endPage = int(point[index+1:])
-                for i in range(startPage, endPage + 1):
-                    pages.append(i)
+                startPage, endPage = map(int, point.split('-'))
+                if startPage > endPage:
+                    tkmb.showerror(title = "Invalid range", message = "The start page is greater than end page in given range")
+                    return []
+                pages.extend(range(startPage, endPage + 1))
             else:
                 pages.append(int(point))
     else:
         tkmb.showerror(title = "Invalid input", message = "Please ensure the input contains only numbers")
     return pages
 
-def splitPDF(pdf, entry):
+def checkBreakPoints(breakPoints, noOfPages):
+    isValid = all(1 <= point <= noOfPages for point in breakPoints)
+    if isValid == False:
+        tkmb.showerror(title = "Error", message = "Cannot perform operations on page that is not in valid range. Valid range : 1 to no. of pages in PDF")
+    return isValid
+
+def getMetadata(pdf, entry):
     if pdf[0] != None:
         reader = pydf.PdfReader(pdf[0])
+        noOfPages = len(reader.pages)
         breakPoints = parseEntry(entry)
-        print(breakPoints)
+        return checkBreakPoints(breakPoints, noOfPages), breakPoints, reader, noOfPages
     else:
-        tkmb.showerror(title = "No PDF found", message = "Please add a pdf before clicking here.")
+        tkmb.showerror(title = "Error", message = "Add a pdf before clicking here.")
+        return False, [], pydf.PdfReader("."), 0
+
+def splitPDF(pdf, entry):
+    flag, breakPoints, reader, noOfPages = getMetadata(pdf, entry)
+    if flag:
+        print(breakPoints)
 
 def deletePages(pdf, entry):
-    if pdf[0] != None:
-        reader = pydf.PdfReader(pdf[0])
-        breakPoints = parseEntry(entry)
-        print(breakPoints)
-    else:
-        tkmb.showerror(title = "No PDF found", message = "Please add a pdf before clicking here.")
+    flag, pagesToDelete, reader, noOfPages = getMetadata(pdf, entry)
+    if flag and pagesToDelete:
+        writer = pydf.PdfWriter()
+        currentPage = 1
+        while (currentPage <= noOfPages):
+            if currentPage not in pagesToDelete:
+                writer.insert_page(reader.pages[currentPage - 1], currentPage)
+            currentPage += 1
+        saveFilePath = asksaveasfilename(defaultextension=".pdf", filetypes=[("PDF Files", "*.pdf")])
+        if saveFilePath:
+            writer.write(saveFilePath)
+            tkmb.showinfo('Deleted Pages', ('The given pages were deleted successfully. PDF stored at : '+saveFilePath))
+        else:
+            tkmb.showerror('Error', 'No destination was specified')
 
 def rotatePages(pdf, entry):
-    if pdf[0] != None:
-        reader = pydf.PdfReader(pdf[0])
-        breakPoints = parseEntry(entry)
+    flag, breakPoints, reader, noOfPages = getMetadata(pdf, entry)
+    if flag:
         print(breakPoints)
-    else:
-        tkmb.showerror(title = "No PDF found", message = "Please add a pdf before clicking here.")
+
